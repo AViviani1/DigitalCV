500687478b2e4d6f70330dbdfe0f3338eeb436b9
Alessandro Viviani <a.viviani246@gmail.com>
2024-03-09 20:13:17 +0100

    remove cache for cv

--- Homepage.py
+++ Homepage.py
@@ -30,8 +30,7 @@ st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 # --- LOAD CSS, PDF & PROFIL PIC ---
 with open(css_file) as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
-
-@st.cache_data 
+ 
 def load_resume(resume_file):
     with open(resume_file, "rb") as pdf_file:
         PDFbyte = pdf_file.read()
